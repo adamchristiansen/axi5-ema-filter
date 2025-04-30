07ca5a2c7d00270ceb3f848943eb8b52a0015b08
Adam Christiansen <hello@adamc.io>
2025-04-30 11:46:54 -0600

    refactor: remove ema_ prefix from test signals

--- ema_filter_tb.tcl
+++ ema_filter_tb.tcl
@@ -10,32 +10,32 @@ set upper [expr $width - 1]
 set lower 0
 
 # Signal names.
-set ema_aclk            "top.ema_filter_tb.ema_aclk"
-set ema_aresetn         "top.ema_filter_tb.ema_aresetn"
-set ema_s_axis_x_tdata  "top.ema_filter_tb.ema_s_axis_x_tdata\[$upper:$lower\]"
-set ema_s_axis_x_tvalid "top.ema_filter_tb.ema_s_axis_x_tvalid"
-set ema_m_axis_y_tdata  "top.ema_filter_tb.ema_m_axis_y_tdata\[$upper:$lower\]"
-set ema_m_axis_y_tvalid "top.ema_filter_tb.ema_m_axis_y_tvalid"
-set ema_alpha           "top.ema_filter_tb.ema_alpha\[$upper:$lower\]"
+set aclk            "top.ema_filter_tb.aclk"
+set aresetn         "top.ema_filter_tb.aresetn"
+set s_axis_x_tdata  "top.ema_filter_tb.s_axis_x_tdata\[$upper:$lower\]"
+set s_axis_x_tvalid "top.ema_filter_tb.s_axis_x_tvalid"
+set m_axis_y_tdata  "top.ema_filter_tb.m_axis_y_tdata\[$upper:$lower\]"
+set m_axis_y_tvalid "top.ema_filter_tb.m_axis_y_tvalid"
+set alpha           "top.ema_filter_tb.alpha\[$upper:$lower\]"
 
 # Add signals.
 gtkwave::/Edit/Insert_Comment "Clock & Reset"
-gtkwave::addSignalsFromList [list $ema_aclk $ema_aresetn]
+gtkwave::addSignalsFromList [list $aclk $aresetn]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "Input Signal"
-gtkwave::addSignalsFromList [list $ema_s_axis_x_tdata $ema_s_axis_x_tvalid]
+gtkwave::addSignalsFromList [list $s_axis_x_tdata $s_axis_x_tvalid]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "Filtered Signal"
-gtkwave::addSignalsFromList [list $ema_m_axis_y_tdata $ema_m_axis_y_tvalid]
+gtkwave::addSignalsFromList [list $m_axis_y_tdata $m_axis_y_tvalid]
 gtkwave::/Edit/Insert_Blank
 gtkwave::/Edit/Insert_Comment "EMA Coefficient"
-gtkwave::addSignalsFromList [list $ema_alpha]
+gtkwave::addSignalsFromList [list $alpha]
 
 # Set all of the listed waveforms to fixed-point.
 #
 # Loop over each waveform instead of highlighting all and manipulating them as a batch. Some
 # commands do not work as a batch and instead only modify the most recently highlighted waveform.
-foreach name [list $ema_s_axis_x_tdata $ema_m_axis_y_tdata $ema_alpha] {
+foreach name [list $s_axis_x_tdata $m_axis_y_tdata $alpha] {
     gtkwave::highlightSignalsFromList [list $name]
     gtkwave::/Edit/Data_Format/Signed_Decimal
     gtkwave::/Edit/Data_Format/Fixed_Point_Shift/Specify $radix
@@ -48,7 +48,7 @@ foreach name [list $ema_s_axis_x_tdata $ema_m_axis_y_tdata $ema_alpha] {
 #
 # Loop over each waveform instead of highlighting all and manipulating them as a batch. Some
 # commands do not work as a batch and instead only modify the most recently highlighted waveform.
-foreach name [list $ema_s_axis_x_tdata $ema_m_axis_y_tdata] {
+foreach name [list $s_axis_x_tdata $m_axis_y_tdata] {
     gtkwave::highlightSignalsFromList [list $name]
     gtkwave::/Edit/Data_Format/Analog/Step
     gtkwave::/Edit/Insert_Analog_Height_Extension
--- ema_filter_tb.vhd
+++ ema_filter_tb.vhd
@@ -16,23 +16,22 @@ is
     constant CLK_PERIOD : time := 10 ns;
 
     --- EMA.
-    constant EMA_DATA_WIDTH: natural := 24;
-    constant EMA_DATA_RADIX: natural := 10;
-    constant EMA_ALPHA_WIDTH: natural := EMA_DATA_WIDTH;
-    constant EMA_ALPHA_RADIX: natural := EMA_DATA_RADIX;
-    signal ema_aclk: std_logic;
-    signal ema_aresetn: std_logic;
-    signal ema_s_axis_x_tdata: std_logic_vector(EMA_DATA_WIDTH - 1 downto 0);
-    signal ema_s_axis_x_tvalid: std_logic;
-    signal ema_m_axis_y_tdata: std_logic_vector(EMA_DATA_WIDTH - 1 downto 0);
-    signal ema_m_axis_y_tvalid: std_logic;
-    signal ema_alpha: std_logic_vector(EMA_ALPHA_WIDTH - 1 downto 0);
+    constant DATA_WIDTH: natural := 24;
+    constant DATA_RADIX: natural := 10;
+    constant ALPHA_WIDTH: natural := DATA_WIDTH;
+    constant ALPHA_RADIX: natural := DATA_RADIX;
+    signal aclk: std_logic;
+    signal aresetn: std_logic;
+    signal s_axis_x_tdata: std_logic_vector(DATA_WIDTH - 1 downto 0);
+    signal s_axis_x_tvalid: std_logic;
+    signal m_axis_y_tdata: std_logic_vector(DATA_WIDTH - 1 downto 0);
+    signal m_axis_y_tvalid: std_logic;
+    signal alpha: std_logic_vector(ALPHA_WIDTH - 1 downto 0);
 
     -- Get `m_axis_y_tdata` as a real number.
     impure function yreal return real is
     begin
-        return to_real(to_sfixed(ema_m_axis_y_tdata,
-            EMA_DATA_WIDTH - EMA_DATA_RADIX - 1, - EMA_DATA_RADIX));
+        return to_real(to_sfixed(m_axis_y_tdata, DATA_WIDTH - DATA_RADIX - 1, - DATA_RADIX));
     end function;
 
     --- Assert that `x` falls in the inclusive interval `[low, high]`.
@@ -52,9 +51,9 @@ begin
 -- Clock.
 aclk_p: process
 begin
-    ema_aclk <= '1';
+    aclk <= '1';
     wait for CLK_PERIOD / 2;
-    ema_aclk <= '0';
+    aclk <= '0';
     wait for CLK_PERIOD / 2;
 end process;
 
@@ -64,16 +63,16 @@ x_p: process
 begin
     -- This alpha has a time constant of 100 ns, which is the time for a step response to reach
     -- `1 - 1/e` (i.e. 63%).
-    ema_alpha <= to_slv(to_sfixed(0.095, EMA_ALPHA_WIDTH - EMA_ALPHA_RADIX - 1, -EMA_ALPHA_RADIX));
+    alpha <= to_slv(to_sfixed(0.095, ALPHA_WIDTH - ALPHA_RADIX - 1, -ALPHA_RADIX));
     -- Unit step input.
-    ema_aresetn <= '0';
-    ema_s_axis_x_tvalid <= '0';
-    ema_s_axis_x_tdata  <= (others => '0');
+    aresetn <= '0';
+    s_axis_x_tvalid <= '0';
+    s_axis_x_tdata  <= (others => '0');
     wait for 10 * CLK_PERIOD;
-    ema_aresetn <= '1';
-    ema_s_axis_x_tvalid <= '1';
+    aresetn <= '1';
+    s_axis_x_tvalid <= '1';
     wait for 10 * CLK_PERIOD;
-    ema_s_axis_x_tdata <= to_slv(to_sfixed(10.0, EMA_DATA_WIDTH - EMA_DATA_RADIX - 1, -EMA_DATA_RADIX));
+    s_axis_x_tdata <= to_slv(to_sfixed(10.0, DATA_WIDTH - DATA_RADIX - 1, -DATA_RADIX));
     wait for 10 * CLK_PERIOD; -- 100 ns.
     assert_interval(yreal, 6.27, 6.37);
     wait for 10 * CLK_PERIOD; -- 100 ns.
@@ -83,15 +82,15 @@ begin
     wait for 150 * CLK_PERIOD;
     assert_interval(yreal, 9.99, 10.0);
     -- Linear input.
-    ema_aresetn <= '0';
-    ema_s_axis_x_tvalid <= '0';
-    ema_s_axis_x_tdata  <= (others => '0');
+    aresetn <= '0';
+    s_axis_x_tvalid <= '0';
+    s_axis_x_tdata  <= (others => '0');
     wait for 10 * CLK_PERIOD;
-    ema_aresetn <= '1';
-    ema_s_axis_x_tvalid <= '1';
+    aresetn <= '1';
+    s_axis_x_tvalid <= '1';
     wait for 10 * CLK_PERIOD;
     while x <= 10.0 loop
-        ema_s_axis_x_tdata <= to_slv(to_sfixed(x, EMA_DATA_WIDTH - EMA_DATA_RADIX - 1, -EMA_DATA_RADIX));
+        s_axis_x_tdata <= to_slv(to_sfixed(x, DATA_WIDTH - DATA_RADIX - 1, -DATA_RADIX));
         wait for CLK_PERIOD;
         x := x + 1.0;
     end loop;
@@ -103,19 +102,19 @@ end process;
 
 dut: entity work.ema_filter
 generic map (
-    DATA_WIDTH => EMA_DATA_WIDTH,
-    DATA_RADIX => EMA_DATA_RADIX,
-    ALPHA_WIDTH => EMA_ALPHA_WIDTH,
-    ALPHA_RADIX => EMA_ALPHA_RADIX
+    DATA_WIDTH => DATA_WIDTH,
+    DATA_RADIX => DATA_RADIX,
+    ALPHA_WIDTH => ALPHA_WIDTH,
+    ALPHA_RADIX => ALPHA_RADIX
 )
 port map (
-    aclk => ema_aclk,
-    aresetn => ema_aresetn,
-    s_axis_x_tdata => ema_s_axis_x_tdata,
-    s_axis_x_tvalid => ema_s_axis_x_tvalid,
-    m_axis_y_tdata => ema_m_axis_y_tdata,
-    m_axis_y_tvalid => ema_m_axis_y_tvalid,
-    alpha => ema_alpha
+    aclk => aclk,
+    aresetn => aresetn,
+    s_axis_x_tdata => s_axis_x_tdata,
+    s_axis_x_tvalid => s_axis_x_tvalid,
+    m_axis_y_tdata => m_axis_y_tdata,
+    m_axis_y_tvalid => m_axis_y_tvalid,
+    alpha => alpha
 );
 
 end behaviour;
